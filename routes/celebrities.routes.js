8d80a5fe0ec0aac9716984f513b96c7145ac55ad
Artur Baumeister <Artur.baumeister@gmail.com>
2022-10-13 16:47:23 +0200

    Done  with bonus, update routes - version 2

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,14 +1,77 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-
 const Celebrity = require("./../models/Celebrity.model");
-// all your routes here
+//* all your routes here
 
-router.get("/create", (req, res, next) => {
-  res.render("celebrities/new-celebrity");
+router.get("/", async (req, res, next) => {
+  try {
+    const celebs = await Celebrity.find();
+    res.render("celebrities/celebrities", { celebrity: celebs });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/create", (req, res, next) => {
+router.get("/create", async (req, res, next) => {
+  try {
+    res.render("celebrities/new-celebrity");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/create", async (req, res, next) => {
+  try {
+    await Celebrity.create({ ...req.body });
+    res.render("celebrities/celebrities");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const celebrity = await Celebrity.findById(id);
+    res.render("celebrities/details", celebrity);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/:id/delete", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Celebrity.findByIdAndDelete(id);
+    res.redirect("/celebrities");
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id/edit", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const celebrity = await Celebrity.findById(id);
+    res.render("celebrities/edit", celebrity);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/:id/edit", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Celebrity.findByIdAndUpdate(id, { ...req.body, $inc: { __v: 1 } });
+    res.redirect(`/celebrities/${id}`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
+
+/* router.post("/create", (req, res, next) => {
   //  const { name, occupation, catchPhrase } = req.body;
   Celebrity.create({ ...req.body })
     .then(() => {
@@ -18,38 +81,32 @@ router.post("/create", (req, res, next) => {
       res.render("celebrities/new-celebrity");
       next(err);
     });
-});
+}); */
 
+/*
 router.get("/", (req, res, next) => {
   Celebrity.find()
     .then(celebrity => {
+      console.log(celebrity);
       res.render("celebrities/celebrities", { celebrity });
     })
     .catch(err => {
       next(err);
     });
-});
+}); */
 
+/*
 router.get("/:id", (req, res, next) => {
   const { id } = req.params;
   Celebrity.findById(id)
     .then(celebrity => {
-      console.log(celebrity);
       res.render("celebrities/details", celebrity);
     })
     .catch(error => next(error));
 });
+*/
 
-router.post("/:id/delete", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    await Celebrity.findByIdAndDelete(id); //.then(() => res.redirect("/celebrities"));
-    res.redirect("/celebrities");
-  } catch (error) {
-    next(error);
-  }
-});
-
+/*
 router.get("/:id/edit", (req, res, next) => {
   const { id } = req.params;
   Celebrity.findById(id)
@@ -58,15 +115,15 @@ router.get("/:id/edit", (req, res, next) => {
     })
     .catch(error => next(error));
 });
+*/
 
+/*
 router.post("/:id/edit", (req, res, next) => {
   const { id } = req.params;
-  console.log(req.body);
   Celebrity.findByIdAndUpdate(id, { ...req.body, $inc: { __v: 1 } })
     .then(() => {
       res.redirect(`/celebrities/${id}`);
     })
     .catch(error => next(error));
 });
-
-module.exports = router;
+*/
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,10 +1,86 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-
 const Celebrity = require("./../models/Celebrity.model");
 const Movie = require("./../models/Movie.model");
 
 // all your routes here
+router.get("/", async (req, res, next) => {
+  try {
+    const movies = await Movie.find();
+    res.render("movies/movies", { movies });
+  } catch (error) {
+    console.log(`Error, getting movies from the db: ${error}`);
+    next(error);
+  }
+});
+
+router.get("/create", async (req, res, next) => {
+  try {
+    const celebs = await Celebrity.find();
+    res.render("movies/new-movie", { celebrities: celebs });
+  } catch (error) {
+    console.log(`Error, getting celebs from the db: ${error}`);
+    next(error);
+  }
+});
+
+router.post("/create", async (req, res, next) => {
+  try {
+    await Movie.create({ ...req.body });
+    res.redirect("/movies");
+  } catch (error) {
+    console.log(`Error, creating movie in the db: ${error}`);
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const movie = await Movie.findById(id).populate("cast");
+    res.render("movies/details", movie);
+  } catch (error) {
+    console.log(`Error, getting movies by ID from the db: ${error}`);
+    next(error);
+  }
+});
+
+router.post("/:id/delete", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Movie.findByIdAndDelete(id);
+    res.redirect("/movies");
+  } catch (error) {
+    console.log(`Error, deleting movie from the db: ${error}`);
+    next(error);
+  }
+});
+
+router.get("/:id/edit", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const movie = await Movie.findById(id).populate("cast");
+    const celebs = await Celebrity.find();
+    res.render("movies/edit", { celebrities: celebs, movie });
+  } catch (error) {
+    console.log(`Error, getting movies or celebs from the db: ${error}`);
+    next(error);
+  }
+});
+
+router.post("/:id/edit", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Movie.findByIdAndUpdate(id, { ...req.body, $inc: { __v: 1 } });
+    res.redirect(`/movies/${id}`);
+  } catch (error) {
+    console.log(`Error, updating movie in the db: ${error}`);
+    next(error);
+  }
+});
+
+module.exports = router;
+/*
 router.get("/create", (req, res, next) => {
   Celebrity.find()
     .then(celebrities => {
@@ -12,7 +88,8 @@ router.get("/create", (req, res, next) => {
     })
     .catch(err => next(err));
 });
-
+*/
+/*
 router.post("/create", (req, res, next) => {
   //  const { title, genre, plot, cast } = req.body;
   Movie.create({ ...req.body }) //* awesome
@@ -20,28 +97,9 @@ router.post("/create", (req, res, next) => {
       res.redirect("/movies");
     })
     .catch(err => next(err));
-});
+});*/
 
 /*
-router.get("/", (req, res, next) => {
-  Movie.find({})
-    .then(movie => {
-      res.render("movies/movies", { movie });
-    })
-    .catch(err => {
-      next(err);
-    });
-}); */ //*Try with async/await
-router.get("/", async (req, res, next) => {
-  try {
-    const movies = await Movie.find(); //console.log(movies);
-    res.render("movies/movies", { movies });
-  } catch (err) {
-    console.log(`Error, getting movies from the db: ${err}`);
-    next(err);
-  }
-});
-
 router.get("/:id", (req, res, next) => {
   const { id } = req.params;
   Movie.findById(id)
@@ -51,18 +109,18 @@ router.get("/:id", (req, res, next) => {
       res.render("movies/details", movie);
     })
     .catch(error => next(error));
-});
+});*/
 
-// /movies/:id/delete	POST	Delete a specific movie
+/*
 router.post("/:id/delete", (req, res, next) => {
   const { id } = req.params;
   Movie.findByIdAndDelete(id)
     .then(() => res.redirect("/movies"))
     .catch(error => next(error));
 });
+*/
 
-/*  /movies/:id/edit	GET	  Show a form to edit a movie
-    /movies/:id/edit	POST	Send the data from the form to this route to update the specific movie */
+/*
 router.get("/:id/edit", (req, res, next) => {
   const { id } = req.params;
   let movie;
@@ -77,7 +135,9 @@ router.get("/:id/edit", (req, res, next) => {
     })
     .catch(error => next(error));
 });
+*/
 
+/*
 router.post("/:id/edit", (req, res, next) => {
   const { id } = req.params;
   //const { title, genre, plot, cast } = req.body;
@@ -87,6 +147,4 @@ router.post("/:id/edit", (req, res, next) => {
       res.redirect(`/movies/${id}`);
     })
     .catch(error => next(error));
-});
-
-module.exports = router;
+}); */
